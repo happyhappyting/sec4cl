f5dc8f0c69387abe9918110992129d66ecb0e06c
happyhappyting <sin_jing_ting@moe.edu.sg>
2026-03-05 09:05:15 +0800

    Update data_q2.js

--- data_q2.js
+++ data_q2.js
@@ -1,1 +1,22 @@
-
+const q2Data = {
+    title: "Q2 公务电邮 (Formal Email)",
+    // 题目例子框
+    example: `
+        <div class="example-box">
+            你看到一张海报，内容是鼓励青少年到全善课后托管中心当义工。义工的职责是为那里的小学生策划并主办课后与假期活动。你认为你学校的学生应该积极响应，在空闲的时候去那里当义工。<br><br>
+            假设你是<span class="hl-yellow">黄海丽</span>，给负责学生理事会的<span class="hl-yellow">王美云</span><span class="bold-text">老师</span>写一则电邮，<span class="hl-yellow">请她协助鼓励学生到全善课后托管中心当义工</span>。<span class="bold-text">你在电邮中要说明学生可以做出的贡献，以及他们会得到的收获。</span>
+        </div>
+    `,
+    // 你的回答框
+    answer: `
+        <div class="essay-box">
+            <span class="bold-text">主题</span>：Compose your own subject based on purpose (E.G. 请求协助鼓励学生到全善课后托管中心当义工)<br><br>
+            <span class="hl-yellow">王</span><span class="bold-text">老师</span>：<br>
+            &emsp;&emsp;您好，本人是XX的顾客／XX区的居民／XX学校的学生。本人今天写电邮的目的是为了＿＿COPY SUBJECT＿＿＿＿。<br><br>
+            &emsp;&emsp;Describe situation with original words, and then write&emsp;&emsp;<br><br>
+            &emsp;&emsp;PEEL X&emsp;&emsp;4 (Usually one issue, two areas (such as impact and suggestions) – 1 area 2 PEELs)<br><br>
+            &emsp;&emsp;<span class="hl-yellow">素闻您一向从善如流，办事效率高。相信您一定会听取建议，做出改善。谢谢！</span><br><br>
+            黄海丽&emsp;&emsp;启
+        </div>
+    `
+};
